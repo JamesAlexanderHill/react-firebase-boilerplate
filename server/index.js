5aa7844a1f794a0c09d042713eef34b14f759584
James Hill <info@jamesalexanderhill.com>
2022-07-09 15:25:48 +1000

    Serve index.html as a fallback

--- server/index.js
+++ server/index.js
@@ -6,12 +6,17 @@ dotenv.config({ path: '../.env' });
 
 const api = require('./routes/api');
 const port = process.env.SVR_PORT || 3001;
-
 const app = express();
 
+/**
+ * Handle requests in order:
+ * 1. Handle any API requests
+ * 2. Serve built ReactJS files
+ * 3. Serve index.html as a fallback for everything else
+ */
 app.use('/api', api);
-
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('*', (req,res) =>res.sendFile(path.join(__dirname, 'public/index.html')));
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
